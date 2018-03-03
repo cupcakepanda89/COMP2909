82d0807e33db882c55a61672abb54f9e1e60dbe4
ktruong <cupcakepanda89@users.noreply.github.com>
2018-03-02 19:14:40 -0800

    mar02 continue

--- mar02/myapp/src/app/app.component.ts
+++ mar02/myapp/src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
+/**
+ * Component architecture pairs up a view and a class which contains logic and data to support the view
+ * selector is an html tag
+ */
+
+/**
+ * Component has two things.
+ * 1. Component decorator which informs app how to process file.
+ * 2. Class which defines data structures and logic
+ */
+
+//1. Component decorator which informs app how to process file.
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+//2. Class which defines data structures and logic
 export class AppComponent {
-  title = 'app';
+  //Here is some data
+  title = 'app'; // variable or property or model
+
+    /**
+     * models - store data
+     */
+
+  // Here is some logic
+  myfunction() {
+    alert("hi");
+  }
 }
