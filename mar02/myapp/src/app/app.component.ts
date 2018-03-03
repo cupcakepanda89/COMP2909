b0327e6986d0c462d485ecf60a31b129b7b9bbca
ktruong <cupcakepanda89@users.noreply.github.com>
2018-03-02 21:09:59 -0800

    table loop

--- mar02/myapp/src/app/app.component.ts
+++ mar02/myapp/src/app/app.component.ts
@@ -8,18 +8,31 @@ export class PlayingCard {
 @Component({
     selector: 'app-root',
     // Multi-line content allowed with back ticks.
-    template: `<h1>Hello world!  {{title}} <br/>
-	        {{card.cardVal}} of {{card.suit}}.</h1>
-               Card Value: <input [(ngModel)]="card.cardVal"><br/>
-               Card Suit:  <input [(ngModel)]="card.suit">`
+    template: `<h1>Hello world!  {{title}}</h1> <br/>
+              <!-- Show cards in unordered list.
+              <ul><li *ngFor="let card of cards">{{card.cardVal}}</li></ul> -->
+
+        <table>
+            <tr>
+                <td>Card Value</td>
+                <td>Card Suit</td>
+            </tr>
+            <tr *ngFor="let card of cards">
+                <td>{{card.cardVal}}</td>
+            </tr>
+        </table>
+    `
 })
 
 export class AppComponent {
     public title = 'This is Angular 4!';
-
-    // Declare a PlayingCard object.
-    public card: PlayingCard = {
-        cardVal: "Ace",
-        suit: "Spades"
-    };
+    // Include card data in collection as public property.
+    public cards = CARDS;
 }
+
+// Define card data.
+let CARDS: PlayingCard[] = [
+    { cardVal: "Ace", suit: "Spades" },
+    { cardVal: "Two", suit: "Clubs" },
+    { cardVal: "Six", suit: "Hearts" },
+];
