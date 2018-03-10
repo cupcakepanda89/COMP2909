f2e0b3f6f2b8db30c68bed35940a5801d01e5193
ktruong <cupcakepanda89@users.noreply.github.com>
2018-03-09 19:50:39 -0800

    modified my name a bit

--- mar09/myapp/src/app/app.component.ts
+++ mar09/myapp/src/app/app.component.ts
@@ -4,13 +4,12 @@ import { MyDataService } from './myDataService';
 @Component({
     selector: 'app-root', //reference the tag that pull the content
     template: `<h1>Hello world!  {{title}}</h1>
+    <p>{{myName}}</p>
     <second-directive [firstName]="author"></second-directive>
     <!--if I want to pass a string [firstName] = "author" like Some String.
     // I have to use single quotation within double quotation
     // [firstName]=" 'some string' " -->
-    <ul><li *ngFor="let name of names">{{name}}</li>
-        <li>{{myName}}</li>
-    </ul>`,
+    <ul><li *ngFor="let name of names">{{name}}</li></ul>`,
     // 'providers' allows you to create and pass an instance
     // of the class to the constructor header.
     providers: [MyDataService]
