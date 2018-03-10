91172505c3b16f53d375eb64df68011fbfc42e63
ktruong <cupcakepanda89@users.noreply.github.com>
2018-03-09 19:25:36 -0800

    create new service

--- mar09/myapp/src/app/app.component.ts
+++ mar09/myapp/src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { MyDataService } from './myDataService';
 
 @Component({
     selector: 'app-root', //reference the tag that pull the content
     template: `<h1>Hello world!  {{title}}</h1>
-    <second-directive></second-directive>`,
+    <second-directive [firstName]="author"></second-directive>
+    <!--if I want to pass a string [firstName] = "author" like Some String.
+    // I have to use single quotation within double quotation
+    // [firstName]=" 'some string' " -->
+    <ul><li *ngFor="let name of names">{{name}}</li></ul>`,
+    // 'providers' allows you to create and pass an instance
+    // of the class to the constructor header.
+    providers: [MyDataService]
+
 })
 export class AppComponent {
     public title = 'This is Angular!';
+    public author = 'Fred';
+    names: Array<any>;
+
+    // Create instance of 'MyDataService' right in the constructor
+    // header.
+    constructor(myDataService: MyDataService) {
+        // Use service to call getNames() method.
+        this.names = myDataService.getNames();
+
+    }
 }
--- mar09/myapp/src/app/newDirective.ts
+++ mar09/myapp/src/app/newDirective.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 // Define second directive.
 @Component({
     // Selector uses lower case with hyphens.
     selector: 'second-directive',
-    template: `<h3>This is the second directive!</h3>
+    template: `<h3>This is the second directive! {{firstName}}</h3>
     <third-directive></third-directive>`,
 })
 export class NewDirective {
+    @Input()
+    firstName: string;
 }
