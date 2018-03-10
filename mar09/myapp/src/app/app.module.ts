d8dbc9b744dd867e315c81934626a49fd6dd87a7
ktruong <cupcakepanda89@users.noreply.github.com>
2018-03-09 20:09:41 -0800

    modified app.module.ts with routing

--- mar09/myapp/src/app/app.module.ts
+++ mar09/myapp/src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule }      from '@angular/core';
+import { FormsModule }    from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NewDirective }  from './newDirective';
 import { ThirdDirective } from './newDirective2';
 import { FahrenheitPipe } from './app.fahrenheitPipe';
 import { AppComponent }  from './app.component';
+import { PageDefault }    from './app.pagedefault';
+import { PageAComponent } from './app.page-a';
+import { PageBComponent } from './app.page-b';
+import { routing }        from './app.routing';
+
 
 @NgModule({
-    imports:      [ BrowserModule ],
-    declarations: [ AppComponent, NewDirective, ThirdDirective,FahrenheitPipe ],
+    imports:      [ BrowserModule, FormsModule, routing ],
+    declarations: [ AppComponent, NewDirective, ThirdDirective,FahrenheitPipe,
+        PageDefault,
+        PageAComponent, PageBComponent],
     bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
