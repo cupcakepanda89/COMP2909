c666c41fe4d17e71ae08c79d43cc8b3bcc4b9df3
ktruong <cupcakepanda89@users.noreply.github.com>
2018-03-16 19:05:25 -0700

    first test

--- mar16/myapp/e2e/app.e2e-spec.ts
+++ mar16/myapp/e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser, element, by } from 'protractor';
 
 describe('myapp App', () => {
   let page: AppPage;
@@ -7,8 +8,21 @@ describe('myapp App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
-  });
+    it('Get more practice working with elements in page', () => {
+        page.navigateTo();
+        // Ensure element with id='fname' has text equalling Jane.
+        expect(element(by.id('fname')).getText()).toEqual('Jane');
+
+        // Get input element and enter the text 'Boston'.
+        var cityInput = element(by.id('cityInput'));
+        cityInput.sendKeys('Boston');
+
+        // Get the button and click it.
+        var msgButton = element(by.id('showMsgButton'));
+        msgButton.click();
+
+        // Test the message that is output after the button is clicked.
+        var messageOutput = element(by.id('messageLabel'));
+        expect(messageOutput.getText()).toEqual('It is always sunny in Boston');
+    });
 });
--- mar16/myapp/src/app/app.component.ts
+++ mar16/myapp/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+   // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title = 'app works!';
+    firstName = 'Jane';
+    message: string;
+    getMessage(_city: string) {
+        this.message = 'It is always sunny in ' + _city;
+    }
 }
