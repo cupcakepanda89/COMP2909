55bba42cb79b3f352beee419270de5a44bc41f0d
ktruong <cupcakepanda89@users.noreply.github.com>
2018-03-16 19:48:51 -0700

    first unit test by rung ng tet

--- mar16/myapp/src/app/app.component.ts
+++ mar16/myapp/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
@@ -7,11 +6,5 @@ import { FormControl, FormGroup } from '@angular/forms';
    // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'app works!';
-    firstName = 'Jane';
-    lastName = 'Truong';
-    message: string;
-    getMessage(_city: string) {
-        this.message = 'It is always sunny in ' + _city;
-    }
+    title = 'app';
 }
