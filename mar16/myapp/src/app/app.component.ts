5e461ad9d8987c9cdf7133ee89ec2df06fd1df18
ktruong <cupcakepanda89@users.noreply.github.com>
2018-03-16 20:15:15 -0700

    create test json file to GET

--- mar16/myapp/src/app/app.component.ts
+++ mar16/myapp/src/app/app.component.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import {  MyNameService } from './app.mynameservice';
 
 @Component({
     selector: 'app-root',
-    templateUrl: './app.component.html',
-    // styleUrls: ['./app.component.css']
+    template: `<button (click)="getSomeData()">Test GET Request</button>
+                <ul>
+                    <li *ngFor="let myData of myNames">{{myData.first}} 
+                    {{myData.last}}</li>
+                </ul>`,
+
+    providers: [MyNameService]
 })
 export class AppComponent {
-    title = 'app';
-    myName = 'Katelyn';
+    myNames: Array<any>;
+    _myDataService: MyNameService;
+
+    // Since we are using a provider above we can receive
+    // an instance through an instructor.
+    constructor(myDataService: MyNameService) {
+        // Store local reference to MyDataService.
+        this._myDataService = myDataService;
+    }
+
+    getSomeData() {
+        this._myDataService.getNames() // call getNames
+        // Subscribe to changes in the observable object
+        // that is returned by getRemoteData.
+            .subscribe(
+                // You basically get three handlers.
+                // 1. Handle successful data.
+                data => {
+                    this.myNames = data
+                    console.log(JSON.stringify(data))
+                },
+                // 2. Handle error.
+                error => {
+                    alert(error)
+                },
+                // 3. Execute final instructions when successful.
+                () => {
+                    console.log("Finished")
+                });
+    }
 }
--- mar16/myapp/src/app/app.module.ts
+++ mar16/myapp/src/app/app.module.ts
@@ -1,16 +1,18 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
+import { FormsModule }   from '@angular/forms';
+import { AppComponent }  from './app.component';
+import { HttpModule }    from '@angular/http';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
     imports: [
-        BrowserModule, FormsModule, ReactiveFormsModule
+        BrowserModule,
+        FormsModule,
+        HttpModule,
     ],
-    providers: [],
+    declarations: [
+        AppComponent,
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
