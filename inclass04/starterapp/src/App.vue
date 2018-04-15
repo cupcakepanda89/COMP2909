2b8c8faabbed167bf15e0bf0db44fd672dc6e600
katelyn truong <katelyntruong@katelyns-MBP.hitronhub.home>
2018-04-15 15:30:25 -0700

    vue js starter

---
+++ inclass04/starterapp/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    Parent
+    <hello-world />
+    <hr>
+    <hello-world />
+  </div>
+</template>
+<script>
+  import HelloWorld from './components/HelloWorld'
+
+  export default {
+    name: 'app',
+    components: {
+      HelloWorld
+    }
+  }
+</script>
+<style>
+  #app {
+    color: #00ff00;
+  }
+</style>
---
+++ inclass04/starterapp/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h4>{{ subtitle}}</h4>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        // Here is a model.
+        msg: 'Welcome to Your Vue.js App',
+        subtitle: 'Katelyn Truong'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  #app {
+    color: #ffa2bf;
+  }
+</style>
